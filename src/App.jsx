e50806192a52e398d853bb89e0963deb3aa2c088
marleigh <volcana@gmail.com>
2022-10-01 14:02:20 -0700

    header basic commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./header/Header"
 
 function App() {
 
   return (
-    <div className="App">
-    test
+    <div>
+      <Header />
     </div>
   )
 }
---
+++ src/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css"
+
+
+export default function Header() {
+    return (
+        <header>
+            header stuff here
+        </header>
+    )
+}
