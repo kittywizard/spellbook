0653072d4384966ac9056846346f8a3a18cd4ab1
marleigh <volcana@gmail.com>
2022-10-09 14:17:36 -0700

    nav updates

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,8 +5,7 @@ import exit from "./assets/exit.svg"
 export default function Nav() {
 
     function openMenu(bool) {
-        const menu = document.querySelector('.nav-list')
-        //do the thing
+        const menu = document.querySelector('.nav-list');
         bool ? menu.style.transform = "translateX(0)" : menu.style.transform = "translateX(-100%)";
     }
 
@@ -15,6 +14,28 @@ export default function Nav() {
             <img src={menu} className="menu-open" onClick={() => openMenu(true)}/>
             <ul className="nav-list">
                 <img src={exit} className="menu-close" onClick={() => openMenu(false)}/>
+                <li>
+                    <div className="char-info">
+                        <p>
+                            <span className="char-info-bold">
+                                Name:
+                            </span> 
+                            Tali
+                        </p>
+                        <p>
+                            <span className="char-info-bold">
+                                Class:
+                            </span> 
+                            Sorcerer
+                        </p>
+                        <p>
+                            <span className="char-info-bold">
+                                Level:
+                            </span> 
+                            1
+                        </p>
+                    </div>
+                </li>
                 <li>
                     <a href="#">home</a>
                 </li>
