04136f1740033ec55375f031b8a4cd7e3aa9a31d
marleigh <volcana@gmail.com>
2022-12-23 13:19:42 -0800

    cleaning up, deleting router for now

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,48 @@ import { useContext } from "react";
 import {Context} from "./Context";
 
 import { useState } from "react";
-import {Route, Routes, Link} from "react-router-dom";
-import Home from "./pages/Home";
-import SpellSelect from "./pages/SpellSelect";
-import Start from "./pages/Start";
 
 const App:React.FC = () => {
+    const  {user, setUser, firstTime, setFirstTime} = useContext(Context);
+
+
+  function formSubmit(event: any) {
+  event.preventDefault();
+  setUser(({
+  name: event.target.name.value,
+  class: event.target.class.value,
+  level: event.target.level.value
+  }));
+  setFirstTime(false);
+  }
 
   return (
     <>
       <Header />
-      <section>
-        <a href="/"> Home</a>
-        <a href="/start">Start</a>
-        <a href="/select">Select</a>
-      </section>
-      
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/start" element={<Start/>} />
-          <Route path="/select" element={<SpellSelect/>} />
-      </Routes>
+
+
+    <section>
+        <form onSubmit={(e) => formSubmit(e)} className="start-form">
+        <label htmlFor="name" className="label">
+            Name: 
+        </label>
+        <input type="text" required name="name"></input>
+        <label htmlFor="class" className="label">
+            Class:
+        </label>
+        <input type="text" required name="class"></input>
+        <label htmlFor="level" className="label">
+            Level:
+        </label>
+        <input type="number" min="1" max="20" name="level"></input>
+
+        <button type="submit" className="btn">
+            Go!
+        </button>
+        </form>
+    </section>    
+    
+
     </>
   )
 }
